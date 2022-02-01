fc7ad7848fdf631ec28d1d7e57e8c011796a37fc
Abdullah Alyabroude <abu-omar.852@hotmail.com>
2022-02-01 18:12:03 +0100

    make it better

--- index.js
+++ index.js
@@ -301,6 +301,6 @@ app.use((err, req, res, next) => {
 })
 // listen for requests
 const port = process.env.PORT || 8080;
-app.listen(port, '0.0.0.0' ,() =>{
-  console.log('Your app is listening on port .' + port);
+app.listen(port, '0.0.0.0',() => {
+ console.log('Listening on Port ' + port);
 });
